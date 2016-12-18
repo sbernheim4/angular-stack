e368402e022d0b803861b9f80d3ca23ab0d109d2
Samuel Bernheim <sambernheim@gmail.com>
2016-12-17 23:21:53 -0500

    Gulp for sass compiling and minifying is complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('compileSass', function () {
-	return gulp.src('./browser/scss/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('browser/css'));
+gulp.task('generateCSS', function () {
+	// the source file of the scss is a main file which just imports all the separate scss files
+	return gulp.src('./browser/scss/index.scss')
+	.pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
+	.pipe(cleanCSS()) // minify the css file
+	.pipe(gulp.dest('./browser/css')); // write the css file to ./browser/css
 });
 
 /* Watch files to have gulp tasks run automatically when saved */
 gulp.task('watch', function() {
-	gulp.watch('./browser/scss/*', ['compileSass']);
+	gulp.watch('./browser/scss/*', ['generateCSS']);
 });
 
 gulp.task('default', ['watch']);
