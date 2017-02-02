ff7b9e24d164bd7984c0e56641bf4d6bf00af839
Samuel Bernheim <sambernheim@gmail.com>
2017-02-02 13:27:05 -0500

    Add navbar directive setup code

---
+++ browser/js/navbar/navbar.js
@@ -0,0 +1,13 @@
+app.directive('navbar', function($rootScope, $state) {
+
+	return {
+		restrict: 'E',
+		scope: {},
+		templateUrl: 'js/navbar/navbar.template.min.html',
+
+		link: function (scope) {
+
+			scope.someVar = "BLAHHHH IS SOME VAR";
+		}
+	};
+});
