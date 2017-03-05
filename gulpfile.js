dc0d53e252ea5e3835b49f978d0c63a8a483ba88
Samuel Bernheim <sambernheim@gmail.com>
2017-03-05 12:03:41 -0500

    Fix misspelled require statement

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-onst gulp = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
