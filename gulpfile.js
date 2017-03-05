8273fc3fb5cca68069717c614cb378f47e113c8f
Samuel Bernheim <sambernheim@gmail.com>
2017-03-05 01:12:04 -0500

    Add more placeholder text

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+onst gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
