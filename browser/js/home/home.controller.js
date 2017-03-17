aab779017fce91a99c8eb1eb3d6b636f10d7cd29
Samuel Bernheim <sambernheim@gmail.com>
2017-03-17 00:26:20 -0400

    Add audio to website

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -2,4 +2,12 @@
 
 app.controller('HomeCtrl', function($scope) {
 
+	$scope.playAudio = function() {
+		var audio = new Audio('../../audio.mp3');
+		audio.load();
+		audio.play();
+	};
+
+	$scope.playAudio();
+
 });
