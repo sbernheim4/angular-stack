2e11768bff06ec03f8cc238b748478bb597db274
Samuel Bernheim <sambernheim@gmail.com>
2017-03-23 19:36:34 -0400

    Remove audio clip

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -2,12 +2,4 @@
 
 app.controller('HomeCtrl', function($scope) {
 
-	$scope.playAudio = function() {
-		var audio = new Audio('../../audio.mp3');
-		audio.load();
-		audio.play();
-	};
-
-	$scope.playAudio();
-
 });
