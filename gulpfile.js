5390a642a21c91bc6c25fc12feb45df92da910be
Samuel Bernheim <sambernheim@gmail.com>
2017-06-08 19:18:02 -0400

    Clean up and organize gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -23,100 +23,97 @@ const livereload = require('gulp-livereload');
 /* Production Builds */
 
 gulp.task('buildCSSProduction', () => {
-	return gulp.src('./browser/scss/index.scss')
-		.pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
-		.pipe(postcss([autoprefixer({browsers: ['> 1%']})]))
-		.pipe(cleanCSS()) // minify the css file
-		.pipe(gulp.dest('./server/public/')) // write the css file to ./server
+    return gulp.src('./browser/scss/index.scss')
+        .pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
+        .pipe(postcss([autoprefixer({browsers: ['> 1%']})]))
+        .pipe(cleanCSS()) // minify the css file
+        .pipe(gulp.dest('./server/public/')) // write the css file to ./server
 });
 
 gulp.task('buildJSProduction', () => {
-	return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
-		.pipe(sourcemaps.init()) // use sourcemaps
-		.pipe(concat('main.js')) // write all the files to a single file called main.js
-		.pipe(babel()) // run babel to use ES6 syntax
-		.pipe(ngAnnotate()) // not quite sure what this does
-		.pipe(uglify()) // minify the js
-		.pipe(sourcemaps.write('./')) // write the source map
-		.pipe(gulp.dest('./server/public')) // write the result of this to ./server/public
+    return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
+        .pipe(sourcemaps.init()) // use sourcemaps
+        .pipe(concat('main.js')) // write all the files to a single file called main.js
+        .pipe(babel()) // run babel to use ES6 syntax
+        .pipe(ngAnnotate()) // not quite sure what this does
+        .pipe(uglify()) // minify the js
+        .pipe(sourcemaps.write('./')) // write the source map
+        .pipe(gulp.dest('./server/public')) // write the result of this to ./server/public
 });
 
 gulp.task('buildHTMLProduction', () => {
-	return gulp.src('./browser/js/**/*.template.html')
-		.pipe(htmlmin({collapseWhitespace: true, removecomments: true}))
-		.pipe(rename(function(path) {
-			path.extname = '.min.html' // change file extention from .html to .min.html
-		}))
-		.pipe(gulp.dest('./browser/js'))
+    return gulp.src('./browser/js/**/*.template.html')
+        .pipe(htmlmin({collapseWhitespace: true, removecomments: true}))
+        .pipe(rename(function(path) {
+            path.extname = '.min.html' // change file extention from .html to .min.html
+        }))
+        .pipe(gulp.dest('./browser/js'))
 });
 
-gulp.task('buildProduction', ['buildHTMLProduction', 'buildCSSProduction', 'buildJSProduction']);
-
-/**********************************************************/
+gulp.task('buildProduction', ['buildHTMLProduction', 'buildCSSProduction', 'buildJSProduction'], function () { buildDone(); });
 
 gulp.task('lintJS', () => {
-	return gulp.src('./browser/js/**/*.js')
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
+    return gulp.src('./browser/js/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('lintCSS', () => {
-	return gulp.src('./browser/scss/*.scss')
-		.pipe(gulpStylelint({
-			reporters: [
-				{formatter: 'string', console: true}
-			]
-		}));
+    return gulp.src('./browser/scss/*.scss')
+        .pipe(gulpStylelint({
+            reporters: [
+                {formatter: 'string', console: true}
+            ]
+        }));
 });
 
-
-gulp.task('print-lint', () => {
-	console.log(chalk.green('\t----------------------'));
-	console.log(chalk.green('\t|       Linting      |'));
-	console.log(chalk.green('\t----------------------'));
-});
+gulp.task('lint', () => {
+    printLint();
+    gulp.start('lintJS');
+    gulp.start('lintCSS');
+})
 /**********************************************************/
 /* Development Builds */
 
 gulp.task('buildCSS', () => {
-	// The source scss file is a main file which just imports all the separate scss files
-	return gulp.src('./browser/scss/index.scss')
-		.pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
-		.pipe(cleanCSS()) // minify the css file
-		.pipe(gulp.dest('./server/public/')) // write the css file to ./server
-		.pipe(livereload()); // reload browser automatically
+    // The source scss file is a main file which just imports all the separate scss files
+    return gulp.src('./browser/scss/index.scss')
+        .pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
+        .pipe(cleanCSS()) // minify the css file
+        .pipe(gulp.dest('./server/public/')) // write the css file to ./server
+        .pipe(livereload()); // reload browser automatically
 });
 
 gulp.task('buildJS', () => {
-	return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
-		.pipe(concat('main.js')) // write all the files to a single file called main.js
-		.pipe(babel()) // run babel to use ES6 syntax
-		.pipe(gulp.dest('./server/public')) // write the result of this to ./server/public
-		.pipe(livereload()); // reload browser automatically
+    return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
+        .pipe(concat('main.js')) // write all the files to a single file called main.js
+        .pipe(babel()) // run babel to use ES6 syntax
+        .pipe(gulp.dest('./server/public')) // write the result of this to ./server/public
+        .pipe(livereload()); // reload browser automatically
 });
 
 gulp.task('buildHTML', () => {
-	// Need the .template.html since using just .html would then create a new
-	// file on each save
-	return gulp.src('./browser/js/**/*.template.html')
-		.pipe(htmlmin(
-			{
-				removeComments: true      // remove comments
-			}))
-		.pipe(rename( (path) => {
-			path.extname = '.min.html' // change file extention from .html to .min.html
-		}))
-		.pipe(gulp.dest('./browser/js'))
-		.pipe(livereload()); // reload browser automatically
+    // Need the .template.html since using just .html would then create a new
+    // file on each save
+    return gulp.src('./browser/js/**/*.template.html')
+        .pipe(htmlmin(
+            {
+                removeComments: true      // remove comments
+            }))
+        .pipe(rename( (path) => {
+            path.extname = '.min.html' // change file extention from .html to .min.html
+        }))
+        .pipe(gulp.dest('./browser/js'))
+        .pipe(livereload()); // reload browser automatically
 })
 
 /* Watch files to have gulp tasks run automatically when saved */
 gulp.task('watch', () => {
-	livereload.listen(); // reload browser automatically on save
-	gulp.watch('./browser/scss/*', ['buildCSS', 'lintCSS']);
-	gulp.watch('./browser/js/**/*.js', ['buildJS'])
-	gulp.watch('./browser/js/**/*.html', ['buildHTML']);
+    livereload.listen(); // reload browser automatically on save
+    gulp.watch('./browser/scss/*', ['buildCSS', 'lintCSS']);
+    gulp.watch('./browser/js/**/*.js', ['buildJS', 'lintJS'])
+    gulp.watch('./browser/js/**/*.html', ['buildHTML']);
 });
 
 /*
@@ -125,4 +122,38 @@ gulp.task('watch', () => {
  * Run buildCSS and buildJS so the app is built/updated without requiring a save
  * in one of the watched files to run the same tasks
  */
-gulp.task('default', ['buildHTML', 'buildCSS', 'buildJS', 'watch', 'lintCSS']);
+gulp.task('default', ['buildHTML', 'buildCSS', 'buildJS', 'watch', 'lint']);
+
+
+
+function buildDone() {
+    console.log(chalk.green.bold(`
+o==================================================o
+|        o---o   o    o  o  o       o--o           |
+|        |   |   |    |  |  |       |   \\          |
+|        o---o   |    |  |  |       |    o         |
+|        |    \\  |    |  |  |       |   \/          |
+|        o----o  o----o  o  o----o  o--o           |
+|                                                  |
+|         o--o   o-----o  o   o  o---o             |
+|         |   \\  |     |  |\\  |  |                 |
+|         |    o |     |  | \\ |  |---o             |
+|         |   \/  |     |  |  \\|  |                 |
+|         o--o   o-----o  o   o  o---o             |
+o==================================================o
+`));
+
+}
+
+function printLint() {
+    console.log(chalk.green.bold(`
+o=====================================================o
+|    o      o  o   o  o-----o  o  o   o    o----o     |
+|    |      |  |\\  |     |     |  |\\  |  \/            |
+|    |      |  | \\ |     |     |  | \\ | o    o---o    |
+|    |      |  |  \\|     |     |  |  \\|  \\     |      |
+|    o---o  o  o   o     o     o  o   o   o----o      |
+o=====================================================o
+    `));
+};
+
